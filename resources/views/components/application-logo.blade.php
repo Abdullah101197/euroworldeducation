8005b8fc3bac8a88c5a721fe750ae199ace21398
abdullah younas <abdullahyounas101197@gmail.com>
2026-07-06 17:35:55 +0500

    Make logo horizontal in admin panel

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->merge(['class' => 'font-bold text-2xl tracking-tighter']) }}>
-    <span class="text-brand-red">EURO</span><span class="text-brand-gold">WORLD</span>
-    <div class="text-xs uppercase tracking-widest opacity-80 mt-1">Education</div>
+<div {{ $attributes->merge(['class' => 'font-bold text-2xl tracking-tighter flex items-center gap-2']) }}>
+    <span><span class="text-brand-red">EURO</span><span class="text-brand-gold">WORLD</span></span>
+    <span class="text-sm uppercase tracking-widest opacity-80 mt-1">Education</span>
 </div>
